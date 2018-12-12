4c355b35523b095b889e944040b452cead411a59
coldfire <amirmohammadkz@gmail.com>
2018-12-12 16:13:53 +0330

    db completed

--- skype.py
+++ skype.py
@@ -9,9 +9,9 @@ def index():
     return render_template('form.html')
 
 
-@app.route('/signup')
+@app.route('/login')
 def login():
-    return render_template('signup.html')
+    return render_template('login.html')
 
 
 @app.route('/process', methods=['POST'])
@@ -35,16 +35,34 @@ def process():
         finally:
             con.close()
 
+
 @app.route('/loginProcess', methods=['POST'])
 def loginProcess():
     if request.method == 'POST':
+        con = sqlite3.connect("skype")
         username = request.form['username']
         password = request.form['password']
-        
+
         if username and password:
-            return jsonify({'name': username})
-        else:
-            return jsonify({'error': 'Missing data!'})
+            try:
+                cur = con.cursor()
+                cur.execute("SELECT username, password FROM users")
+                users = cur.fetchall()
+                user = [i for i in users if i[0] == username]
+                if (len(user) > 0):
+                    if (str(user[0][1]) == password):
+                        return jsonify({'name': username})
+                    else:
+                        return jsonify({'error': 'username or password is incorrect!'})
+                else:
+                    return jsonify({'error': 'username or password is incorrect!'})
+                # con.commit()
+            except Exception as e:
+                print(e)
+                return jsonify({'error': 'something went wrong!'})
+            finally:
+                con.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True)

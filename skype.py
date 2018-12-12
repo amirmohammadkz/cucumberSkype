3080924b32ec4fb2557537df96cafe10ed28e3df
SaDeGhTb <stabasi76@gmail.com>
2018-12-12 15:50:40 +0330

    addcontact Added

--- skype.py
+++ skype.py
@@ -35,6 +35,10 @@ def process():
         finally:
             con.close()
 
+@app.route('/addcontact')
+def addcontact(): 
+    return render_template('addcontact.html')
+
 @app.route('/loginProcess', methods=['POST'])
 def loginProcess():
     if request.method == 'POST':
@@ -45,6 +49,24 @@ def loginProcess():
             return jsonify({'name': username})
         else:
             return jsonify({'error': 'Missing data!'})
+
+@app.route('/addcontactprocess', methods=['POST'])
+def addContactProcess():
+    if request.method == 'POST':
+        con = sqlite3.connect("skype")
+        try:
+            contact = request.form['contact']
+            if contact:
+                cur = con.cursor()
+                myid = cur.execute("SELECT id FROM users WHERE username=?'' ",(username))
+                toid = cur.execute("SELECT id FROM users WHERE username=?'' ",(contact))
+                cur.execute("INSERT INTO contacts (from,to) VALUES(?,?)", (myid, toid))
+                con.commit()
+                return jsonify({'contact': contact})
+            else:
+                return jsonify({'error': 'Missing data!'})
+        except:
+            con.rollback()
 
 if __name__ == '__main__':
     app.run(debug=True)

11045ca8d76428dafa00504e72756010c30febd1
coldfire <amirmohammadkz@gmail.com>
2018-12-12 15:19:34 +0330

    first

---
+++ skype.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+
+@app.route('/signup')
+def login():
+    return render_template('signup.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    if request.method == 'POST':
+        con = sqlite3.connect("skype")
+        try:
+            username = request.form['username']
+            password = request.form['password']
+
+            if username and password:
+                # with sqlite3.connect("skype") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO users (username,password) VALUES(?,?)", (username, password))
+                con.commit()
+                return jsonify({'name': username})
+            else:
+                return jsonify({'error': 'Missing data!'})
+        except:
+            con.rollback()
+        finally:
+            con.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
